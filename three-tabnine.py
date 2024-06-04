d0310e243d962a3077b248a89ef0c47625ac4b8b
James Pichardo <sherlock@221b.me>
2024-06-03 20:32:52 -0400

    adding investigation id to task execution

--- three-tabnine.py
+++ three-tabnine.py
@@ -14,14 +14,16 @@ def list_sensors(manager):
         hostname = sensor.hostname()
         print(f"Sensor ID: {sensor.sid}, Hostname: {hostname}")
 
-def execute_task(manager, sensor_id, task):
+def execute_task(manager, sensor_id, task, investigation_id=None):
     sensor = manager.sensor(sensor_id)
-    response = sensor.task([task])
+    response = sensor.task([task], inv_id=investigation_id)
+    print(response)
 
 def main():
     parser = argparse.ArgumentParser(description='Interact with LimaCharlie sensors.')
     parser.add_argument('--list', action='store_true', help='List all online sensors')
     parser.add_argument('--execute', metavar='SENSOR_ID,TASK', help='Execute a task on a given sensor')
+    parser.add_argument('--inv_id', metavar='INVESTIGATION_ID', help='Specify an investigation ID')
     args = parser.parse_args()
 
     api_key = get_api_key()
@@ -32,7 +34,7 @@ def main():
         list_sensors(manager)
     elif args.execute:
         sensor_id, task = args.execute.split(',')
-        execute_task(manager, sensor_id, task)
+        execute_task(manager, sensor_id, task, args.inv_id)
 
 if __name__ == '__main__':
     main()
